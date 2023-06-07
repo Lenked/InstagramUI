6c88beb5f246b739ebbbb6cf9906ba7d6cfd51ab
Evrard237-sudo <114961933+Evrard237-sudo@users.noreply.github.com>
2023-06-07 16:26:18 +0100

    first commit

---
+++ app/src/main/java/com/exemple/instagramui/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.exemple.instagramui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.exemple.instagramui.ui.theme.InstagramUITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ProfileScreen()
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ProfileScreen()
+}
